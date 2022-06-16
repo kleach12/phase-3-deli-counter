1a26f4e440c064641aace153916fec7e356180fa
Kale Leach <kleach1212@gmail.com>
2022-06-15 18:02:29 -0700

    commit tree d2fb388ea4c8aae94cc270596b47705582a4b83b

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    katz_deli.map.with_index(1) do |name, place|
+      current_line << " #{place}. #{name}"
+    end
+    puts current_line 
+  end
+end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{deli[0]}."
+    deli.shift()
+  end
+end
